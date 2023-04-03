a2c9af5d1fcbcbd2f0c995a280b1d00f72f90331
Noah Shinn <93103423+noahshinn024@users.noreply.github.com>
2023-04-02 22:20:04 -0400

    .

--- run_simple.sh
+++ run_simple.sh
@@ -1,9 +1,9 @@
 python main.py \
-  --run_name "simple_mbpp_rs" \
+  --run_name "simple_human_eval_py_logging" \
   --root_dir "root" \
-  --dataset_path ./benchmarks/mbpp-rs.jsonl \
+  --dataset_path ./benchmarks/humaneval-py.jsonl.gz \
   --strategy "simple" \
-  --language "rs" \
+  --language "py" \
   --model "gpt-4" \
   --pass_at_k "1" \
   --max_iters "1" \
