5fd6ff89c67275be2b9ac9a54f553880a3908bc0
elleven11 <federico.cassano@federico.codes>
2023-04-04 00:27:20 -0400

    leet shape code

--- executors/factory.py
+++ executors/factory.py
@@ -1,11 +1,18 @@
-from .py_executor import PyExecutor
-from .rs_executor import RsExecutor
+from .py_executor import PyExecutor, PyLeetExecutor
+from .rs_executor import RsExecutor, RsLeetExecutor
 from .executor_types import Executor
 
-def executor_factory(lang: str) -> Executor:
+
+def executor_factory(lang: str, is_leet: bool = False) -> Executor:
     if lang == "py" or lang == "python":
-        return PyExecutor()
+        if is_leet:
+            return PyLeetExecutor()
+        else:
+            return PyExecutor()
     elif lang == "rs" or lang == "rust":
-        return RsExecutor()
+        if is_leet:
+            return RsLeetExecutor()
+        else:
+            return RsExecutor()
     else:
         raise ValueError(f"Invalid language for executor: {lang}")
