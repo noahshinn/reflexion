818fc53c89526bb61cd360e6fc0173506bd059ba
Beck LaBash <55890162+becklabs@users.noreply.github.com>
2023-04-14 22:46:47 -0400

    Add back dynamic imports

--- executors/leet_executor.py
+++ executors/leet_executor.py
@@ -2,18 +2,13 @@ from typing import List
 
 from .executor_types import ExecuteResult, Executor
 from .executor_utils import to_jsonl
-from .leetcode_env.leetcode_env.utils import SubmissionFormatter
-
-from .leetcode_env.leetcode_env.environment import LeetCodeEnv
-from .leetcode_env.leetcode_env.leetcode_types import ProgrammingLanguage, LeetCodeSubmission
-from .leetcode_env.leetcode_env.utils import id_from_slug
-
 from datetime import datetime
 
-class LeetExecutor(Executor):
-    from .leetcode_env.leetcode_env.utils import SubmissionFormatter
-    from .leetcode_env.leetcode_env.leetcode_types import ProgrammingLanguage
+from .leetcode_env.leetcode_env.utils import SubmissionFormatter
+from .leetcode_env.leetcode_env.leetcode_types import ProgrammingLanguage
 
+class LeetExecutor(Executor):
+    
     def __init__(self, lang: ProgrammingLanguage, executor: Executor, formatter: SubmissionFormatter):
         from .leetcode_env.leetcode_env.environment import LeetCodeEnv
         self.lang = lang
@@ -26,19 +21,32 @@ class LeetExecutor(Executor):
         return self.executor.execute(func, tests, timeout)
 
     def evaluate(self, name: str, func: str, test: str, timeout: int = 5) -> bool:
+        from .leetcode_env.leetcode_env.leetcode_types import LeetCodeSubmission
+        from .leetcode_env.leetcode_env.utils import id_from_slug
         print(f'Timeout is {timeout} seconds')
         leetcode_formatted_func = self.formatter.to_leetcode(func)
         print('----------------- LEETCODE SUBMISSION ------------------')
         print(leetcode_formatted_func)
         print('--------------------------------------------------------')
         submission = LeetCodeSubmission(
-            code=self.formatter.to_leetcode(func),
+            code=leetcode_formatted_func,
             lang=self.lang,
             question_id=id_from_slug(name, self.env.api_instance),
             question_slug=name,
             timeout=timeout
         )
 
-        _, reward, _, _ = self.env.step(submission)
+        status, reward, _, info = self.env.step(submission)
+
+        print('----------------- LEETCODE SUBMISSION ------------------')
+        print(status)
+        print('--------------------------------------------------------')
+
+        to_jsonl({
+            'name': name,
+            'status': status,
+            'reward': reward,
+            'info': info
+        }, self.name)
 
         return reward
