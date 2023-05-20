cf799785a6eb3c8dd1f5322ca443cb245ddf2f0a
Beck LaBash <55890162+becklabs@users.noreply.github.com>
2023-05-20 15:17:57 -0400

    NBs and README

--- hotpotqa_runs/util.py
+++ hotpotqa_runs/util.py
@@ -31,7 +31,37 @@ Trial summary: Correct: {len(correct)}, Incorrect: {len(incorrect)}
 
     return log
 
+def summarize_react_trial(agents):
+    correct = [a for a in agents if a.is_correct()]
+    halted = [a for a in agents if a.is_halted()]
+    incorrect = [a for a in agents if a.is_finished() and not a.is_correct()]
+    return correct, incorrect, halted
+
+def log_react_trial(agents, trial_n):
+    correct, incorrect, halted = summarize_react_trial(agents)
+
+    log = f"""
+########################################
+BEGIN TRIAL {trial_n}
+Trial summary: Correct: {len(correct)}, Incorrect: {len(incorrect)}, Halted: {len(halted)}
+#######################################
+"""
+
+    log += '------------- BEGIN CORRECT AGENTS -------------\n\n'
+    for agent in correct:
+        log += remove_fewshot(agent._build_agent_prompt()) + f'\nCorrect answer: {agent.key}\n\n'
+
+    log += '------------- BEGIN INCORRECT AGENTS -----------\n\n'
+    for agent in incorrect:
+        log += remove_fewshot(agent._build_agent_prompt()) + f'\nCorrect answer: {agent.key}\n\n'
+
+    log += '------------- BEGIN HALTED AGENTS -----------\n\n'
+    for agent in halted:
+        log += remove_fewshot(agent._build_agent_prompt()) + f'\nCorrect answer: {agent.key}\n\n'
+
+    return log
+
 def save_agents(agents, dir: str):
     os.makedirs(dir, exist_ok=True)
     for i, agent in enumerate(agents):
-        joblib.dump(agent, f'{dir}/{i}.joblib')
+        joblib.dump(agent, os.path.join(dir, f'{i}.joblib'))
