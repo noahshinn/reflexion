dbfc7c6a4f33df49fa52cbccd99e659799bbf8c8
cassanof <federico.cassano@federico.codes>
2023-06-18 03:08:17 -0700

    runner

---
+++ programming_runs/run_reflexion_startchat.sh
@@ -0,0 +1,10 @@
+python main.py \
+  --run_name "reflexion_star_chat" \
+  --root_dir "root" \
+  --dataset_path ./benchmarks/humaneval-py.jsonl \
+  --strategy "reflexion" \
+  --language "py" \
+  --model "star-chat" \
+  --pass_at_k "1" \
+  --max_iters "3" \
+  --verbose
